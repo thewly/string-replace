4c4d7edfd22e49ca48ef2ea5718f3fc5608f1d56
Matthew Cole <colematthewg@gmail.com>
2019-08-21 21:25:24 -0500

    functional

--- app.js
+++ app.js
@@ -12,29 +12,46 @@ app.get('/', (req, res) => {
       'x-timestamp': Date.now(),
       'x-sent': true
     }
-  }
+  };
 
   var fileName = 'form.html';
   res.sendFile(fileName, options, function (err) {
     if (err) {
-      next(err)
+      console.error('err', err);
+      next(err);
     } else {
-      console.log('Sent:', fileName)
+      console.log('Sent:', fileName);
     }
-  })
+  });
 });
 
 app.get('/replace', (req, res) => {
   console.log('query', req.query);
 
-  // fs.readFile('2786_EE-SS.html', 'utf8', function (err,data) {
-  fs.readFile('input.html', 'utf8', function (err, data) {
+  fs.readFile('Template_EE-SS_modified.html', 'utf8', function (err,data) {
+  // fs.readFile('input.html', 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     }
-    var result = data.replace(/!@#\$/g, req.query.garbage);
-    var result = result.replace(/asdf/g, req.query.text);
-    var result = result.replace(/         /g, req.query.whitespace);
+
+    var regex = /#000000/gi;
+
+    var result = data.replace('schoollink', req.query.schoollink);
+    // result = result.replace(/asdf/g, req.query.text);
+    result = result.replace('schoolname', req.query.schoolname);
+    result = result.replace('School Name', req.query.schoolnamehead);
+    result = result.replace('111111', req.query.UTM);
+    result = result.replace('222222', req.query.SMI);
+    result = result.replace('[Nickname]', req.query.nickname);
+    result = result.replace('**Day**', req.query.day);
+    result = result.replace('**Month**', req.query.month);
+    result = result.replace('**#**', req.query.number);
+    result = result.replace('**00**', req.query.starthour);
+    result = result.replace('**a.m.**', req.query.am);
+    result = result.replace('**00**', req.query.endhour);
+    result = result.replace('**p.m.**', req.query.pm);
+    result = result.replace('**Location**', req.query.location);
+    result = result.replace(regex, req.query.buttoncolor);
 
     fs.writeFileSync('output.html', result);
     res.download('output.html');
@@ -42,4 +59,4 @@ app.get('/replace', (req, res) => {
 
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
