091ddfffdb78a0767fd3814d3961e8f0f7c87b98
Matthew Cole <colematthewg@gmail.com>
2019-08-22 08:05:24 -0500

    added snippet of code from IGTBOK to try to fix the Heroku post ERR

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 80;
+// const port = 80;
 const fs = require('fs');
 const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const URL = process.env.NODE_ENV === 'production' ? 'https://balfour-email.herokuapp.com' : 'http://localhost:3001'
+
+// const db = require('./models')
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 
 app.get('/', (req, res) => {
   var options = {
@@ -60,4 +74,4 @@ app.get('/replace', (req, res) => {
 
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
